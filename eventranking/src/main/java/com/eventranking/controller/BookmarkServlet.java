c2a11d589e236a38d95ab7c54a033a4e0d312d7f
Eddyoung0 <ashishlimbu38@gmail.com>
2026-05-10 17:14:28 +0545

    Describe the change to VoteServlet and BookmarkServlet

--- eventranking/src/main/java/com/eventranking/controller/BookmarkServlet.java
+++ eventranking/src/main/java/com/eventranking/controller/BookmarkServlet.java
@@ -1,3 +1,18 @@
-public class BookmarkServlet {
-    
+package com.eventranking.controller;
+
+import com.eventranking.dao.BookmarkDAO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet({"/bookmark"})
+public class BookmarkServlet extends HttpServlet {
+   private final BookmarkDAO bookmarkDAO = new BookmarkDAO();
+
+   public BookmarkServlet() {
+   }
 }
--- eventranking/src/main/java/com/eventranking/controller/VoteServlet.java
+++ eventranking/src/main/java/com/eventranking/controller/VoteServlet.java
@@ -1,3 +1,18 @@
-public class VoteServlet {
-    
+package com.eventranking.controller;
+
+import com.eventranking.dao.VoteDAO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet({"/vote"})
+public class VoteServlet extends HttpServlet {
+   private final VoteDAO voteDAO = new VoteDAO();
+
+   public VoteServlet() {
+   }
 }
